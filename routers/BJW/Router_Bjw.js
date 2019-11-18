edf73df591ec632ba0e0fc090dd9c935613a9372
WishMeLzzz <wishmelzzz@WishMeLzzzdeMacBook-Pro.local>
2019-11-18 20:03:43 +0800

    add

--- routers/BJW/Router_Bjw.js
+++ routers/BJW/Router_Bjw.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const router = express.Router()
 router.get("/", (req, res) => {
-    res.send("aaa123-------------sdas+++++++++++")
+    // res.send("aaa123-------------sdas+++++++++++")
+    res.json({
+        code:"222",
+        masg:"dsa"
+    })
 })
 module.exports = router;
