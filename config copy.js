e5e7feef9d603ae3ebe9da6a8dfec0c3b5fb27f5
WishMeLzzz <wishmelzzz@WishMeLzzzdeMacBook-Pro.local>
2019-11-18 20:10:58 +0800

    add

---
+++ config copy.js
@@ -0,0 +1,13 @@
+module.exports = {
+    server: {
+        port: 9999,
+        prefix: "/api/v1"
+    },
+    db: {
+        host: 'cdb-njokm572.bj.tencentcdb.com',
+        port: 10165,
+        user: 'root',
+        password: 'myiszz00',
+        database: 'banana',
+    }
+}
