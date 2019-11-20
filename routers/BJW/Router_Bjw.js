ec81fc6e1d732f948df233c4a47ee632942495ad
BaiJunWu <117929888@qq.com>
2019-11-20 09:23:06 +0800

    广场模块渲染

--- routers/BJW/Router_Bjw.js
+++ routers/BJW/Router_Bjw.js
@@ -1,10 +1,21 @@
 const express = require("express")
 const router = express.Router()
-router.get("/", (req, res) => {
-    // res.send("aaa123-------------sdas+++++++++++")
-    res.json({
-        code:"222",
-        masg:"dsa"
+const db = require("../../db")
+router.get("/plaza", (req, res) => {
+    let sql = `select * from plaza a, plazaimg b, plazalike c where a.id = b.plaza_id and a.Me_id = c.Me_id`
+    db.query(sql, (err, data) => {
+        // console.log(data)
+        if (err) {
+            res.json({
+                'code': 500,
+                'error': err
+            })
+        } else {
+            res.json({
+                'code': 200,
+                'data': data
+            })
+        }
     })
 })
 module.exports = router;
