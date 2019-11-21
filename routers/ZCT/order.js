f400d07b345f431adb486ff4ac94d50bc6014778
ZhangChaoTang <3223376007@qq.com>
2019-11-21 20:55:50 +0800

    '下单,创建订单'

--- routers/ZCT/order.js
+++ routers/ZCT/order.js
@@ -100,7 +100,7 @@ router.put("/order_st/:id", (req, res) => {
 })
 
 // 退款
-router.put("/order_st/refund/:id",(req,res)=>{
+router.put("/order_st/refund/:id", (req, res) => {
   let id = req.params.id;
   let order_type = req.body.type
   let sql = 'UPDATE `order` SET return_state = ? WHERE id =?'
@@ -118,4 +118,114 @@ router.put("/order_st/refund/:id",(req,res)=>{
     }
   })
 })
-module.exports = router;
+
+// 准备创建订单 查询商品信息
+router.get("/purchase/:id", (req, res) => {
+  let ids = req.params.id.split(",")
+  // console.log(ids)
+  let sql = 'SELECT * FROM menu WHERE id IN(?)'
+  db.query(sql, [ids], (error, data) => {
+    if (error) {
+      console.log(error)
+      return res.json({
+        ok: 0,
+        error: error
+      })
+    } else {
+      // console.log(data)
+      res.json({
+        ok: 1,
+        data: data
+      })
+    }
+  })
+})
+
+// // 创建订单
+router.post('/createorder_st', (req, res) => {
+  // 购物车id
+  let cartid = req.body.data.cartid
+  // 当前时间秒
+  let time = Date.parse(new Date()) / 1000;
+  // 商品id
+  let commodityId = req.body.data.ids
+  // 支付金额
+  let price = req.body.data.totalPrice
+  // 用户id
+  let userid = 1 //req.body.userid
+  // 姓名
+  let name = req.body.data.users_name
+  // 手机号
+  let mobile = req.body.data.users_mobile
+  // 地址
+  let site = req.body.data.dormitory + req.body.data.dormitory_no
+  // console.log(req.body.dormitory)
+  // 获取当前时间戳毫秒
+  var timestamp = Date.parse(new Date());
+  // 生成七个随机数
+  let num = Math.random().toString().substring(2, 9);
+  // 拼接成订单号
+  let order_num = timestamp + num;
+  // // 添加数据
+  let arr = [order_num, time, price, 0, userid, 1, 0, 0, name, mobile, site]
+  // console.log(arr)
+  db.query('insert into `order`(order_num,add_time,price,state,user_id,classify_State,return_state,pay_method,shr_name,shr_mobile,shr_address) values(?)', [arr], (error, data) => {
+    if (error) {
+      // console.log(error)
+      return res.json({
+        ok: 0,
+        msg: "生成订单失败"
+      })
+    } else {
+      // 查询添加的订单号的id
+      db.query('select id from `order` where order_num =?', order_num, (error2, data2) => {
+        if (error2) {
+          console.log(error)
+        }
+        let orderId = data2[0].id
+        let iscorrect = []
+        commodityId.forEach(item => {
+          db.query('insert into order_stdetail(order_id,menu_id) values(?,?)', [orderId, item], (error3, data3) => {
+            if (error3) {
+              console.log(error3)
+              return res.json({
+                ok: 0,
+                msg: "创建失败"
+              })
+            } else {
+              iscorrect.push(data3[0])
+            }
+          })
+        })
+        // 判断
+        iscorrect.forEach(item => {
+          if (!item.affectedRows) {
+            return res.json({
+              ok: 0,
+              msg: "创建失败"
+            })
+          }
+        })
+
+        db.query("DELETE FROM stcare WHERE id IN(?)", [cartid], (error4, data4) => {
+          if (error4) {
+            console.log(error4)
+            return res.json({
+              ok: 0,
+              msg: "创建失败"
+            })
+          } else {
+            res.json({
+              ok: 1,
+              msg: "创建成功"
+            })
+          }
+        })
+
+      })
+    }
+  })
+
+})
+
+module.exports = router
