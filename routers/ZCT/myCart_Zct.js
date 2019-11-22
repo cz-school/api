06eeec92f4e39c76303e1bce0412843dcc00c275
ZhangChaoTang <3223376007@qq.com>
2019-11-22 20:00:51 +0800

    'myCart修改'

--- routers/ZCT/myCart_Zct.js
+++ routers/ZCT/myCart_Zct.js
@@ -4,7 +4,6 @@ const db = require("../../db")
 // 获取食堂购物车信息
 router.get('/myCart_st', (req, res) => {
   let userID = 1;  //req.user.id
-
   let sql = 'SELECT * FROM stcare WHERE user_id = ?'
 
   db.query(sql, userID, (error, data) => {
