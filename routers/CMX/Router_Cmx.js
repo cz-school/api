a910b316ef100c11b245264f700c538d246524e0
车梦肖 <2767293698@qq.com>
2019-11-23 15:35:58 +0800

    update:11-23 图片上传

--- routers/CMX/Router_Cmx.js
+++ routers/CMX/Router_Cmx.js
@@ -17,93 +17,34 @@ let client = new OSS({
     //  存储仓库名字
     bucket: 'zhangchaotang',
 });
-
-// 根据用户登录id获取个人用户信息
-router.get('/self_info/:id(\\d+)', (req, res) => {
-    let useId = req.params.id
-    // console.log(useId)
-    let sql = `select * from users where id= ?`
-    conn.query(sql, useId, (error, data) => {
-        if (error) {
-            res.json({
-                ok: 0,
-                error: error
-            })
-        }
-        res.json({
-            ok: 1,
-            data: data[0]
-        })
-    })
-})
-
 // 获取所有的我的tag标签
-router.get('/our_tag/:id(\\d+)', (req, res) => {
+router.get('/ourTag/:id(\\d+)', (req, res) => {
     let id = req.params.id
-    let sql = `select tag_our_name from self_tag where id=?`
+    let sql = `select tag_our_name from self_tag where id=? `
     conn.query(sql, id, (error, data) => {
         if (error) {
             res.json({
                 ok: 0,
                 error: error
             })
-            return
         }
         res.json({
             ok: 1,
             data: data[0]
         })
-        return
     })
 })
 // 通过用户id来更新我的标签
-router.post('/add_our_tag/:id(\\d+)', (req, res) => {
+router.post('/add_our_tags/:id(\\d+)', (req, res) => {
     let data = JSON.parse(req.body.tags)
     let data1 = []
     data.forEach(v => {
         data1.push(v.tag_name)
     });
     let aa = data1.join(',')
-
     let id = req.body.id
     let sql = `UPDATE self_tag set tag_our_name =? where user_id =?`
     conn.query(sql, [aa, id], (error, data) => {
-        if (error) {
-            res.json({
-                ok: 0,
-                error: error
-            })
-            return
-        }
-        res.json({
-            ok: 1,
-        })
-    })
-})
-// 获取所有运动标签
-router.get('/our_sports/:id(\\d+)', (req, res) => {
-    let id = req.params.id
-    let sql = `select tag_sport_name from self_tag where id=?`
-    conn.query(sql, id, (error, data) => {
-        if (error) {
-            res.json({
-                ok: 0,
-                error: error
-            })
-        }
-        res.json({
-            ok: 1,
-            data: data[0]
-        })
-    })
-})
-// 通过用户id来更新运动标签
-router.post('/add_sports_tag/:id(\\d+)', (req, res) => {
-    let data = req.body.tags
-    let id = req.body.id
-    let sql = `UPDATE self_tag set tag_sport_name =? where user_id =?`
-    conn.query(sql, [data, id], (error, data) => {
-        console.log(data)
         if (error) {
             res.json({
                 ok: 0,
@@ -134,7 +75,7 @@ router.get('/our_music/:id(\\d+)', (req, res) => {
     })
 })
 // 通过用户id来更新音乐标签
-router.post('/add_music_tag/:id(\\d+)', (req, res) => {
+router.post('/add_music_sportstag/:id(\\d+)', (req, res) => {
     let data = JSON.parse(req.body.tags)
     let data1 = []
     data.forEach(v => {
@@ -157,7 +98,46 @@ router.post('/add_music_tag/:id(\\d+)', (req, res) => {
         })
     })
 })
-
+// // 获取所有运动标签
+router.get('/our_sports/:id(\\d+)', (req, res) => {
+    let id = req.params.id
+    let sql = `select tag_sport_name from self_tag where id=? `
+    conn.query(sql, id, (error, data) => {
+        if (error) {
+            res.json({
+                ok: 0,
+                error: error
+            })
+        }
+        res.json({
+            ok: 1,
+            data: data[0]
+        })
+    })
+})
+// 通过用户id来更新运动标签
+router.post('/add_sports_tag/:id(\\d+)', (req, res) => {
+    let data = JSON.parse(req.body.tags)
+    let data1 = []
+    data.forEach(v => {
+        data1.push(v.tag_name)
+    });
+    let aa = data1.join(',')
+    let id = req.body.id
+    let sql = `UPDATE self_tag set tag_sport_name =? where user_id =?`
+    conn.query(sql, [aa, id], (error, data) => {
+        if (error) {
+            res.json({
+                ok: 0,
+                error: error
+            })
+            return
+        }
+        res.json({
+            ok: 1,
+        })
+    })
+})
 // // 获取所有食物标签
 router.get('/our_foods/:id(\\d+)', (req, res) => {
     let id = req.params.id
@@ -199,6 +179,29 @@ router.post('/add_foods_tag/:id(\\d+)', (req, res) => {
         })
     })
 })
+// 通过用户id来更新游戏标签
+router.post('/add_games_tag/:id(\\d+)', (req, res) => {
+    let data = JSON.parse(req.body.tags)
+    let data1 = []
+    data.forEach(v => {
+        data1.push(v.tag_name)
+    });
+    let aa = data1.join(',')
+    let id = req.body.id
+    let sql = `UPDATE self_tag set tag_games_name =? where user_id =?`
+    conn.query(sql, [aa, id], (error, data) => {
+        if (error) {
+            res.json({
+                ok: 0,
+                error: error
+            })
+            return
+        }
+        res.json({
+            ok: 1,
+        })
+    })
+})
 // 获取所有游戏标签
 router.get('/our_games/:id(\\d+)', (req, res) => {
     let id = req.params.id
@@ -216,10 +219,10 @@ router.get('/our_games/:id(\\d+)', (req, res) => {
         })
     })
 })
-// 通过用户id来更新运动标签
-router.get('/our_sports/:id(\\d+)', (req, res) => {
+// 根据用户登录id获取个人用户信息
+router.get('/selfInfo/:id(\\d+)', (req, res) => {
     let id = req.params.id
-    let sql = `select tag_sport_name from self_tag where id =?`
+    let sql = `select * from users where id=? `
     conn.query(sql, id, (error, data) => {
         if (error) {
             res.json({
